47ee27bb8849e725153cac9e1696ef58f270b786
nazmulhasan7447 <nazmulhasan747406@gmail.com>
2023-04-01 05:18:39 +0600

    fixed missing title of add new product modal and added missing product details modal

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -145,13 +145,44 @@ const ProductList = () => {
         </div>
       </AppModal>
 
+      <AppModal id="user-details">
+        <div class="modal-dialog user-details-card modal-lg">
+          <div class="modal-content">
+            <div class="modal-header">
+              {/* <h1 class="modal-title fs-5" id="exampleModalLabel">
+              User
+            </h1> */}
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body"></div>
+            {/* <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" class="btn btn-primary">
+              Send message
+            </button>
+          </div> */}
+          </div>
+        </div>
+      </AppModal>
+
       {/* app modal for adding new user/admin */}
       <AppModal id="add-new-user">
         <div class="modal-dialog user-details-card modal-lg">
           <div class="modal-content">
             <div class="modal-header">
               <h1 class="modal-title fs-5" id="exampleModalLabel">
-                Add new customer
+                Add new product
               </h1>
               <button
                 type="button"
